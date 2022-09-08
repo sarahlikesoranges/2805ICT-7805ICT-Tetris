0e6793c0fbabd6c4d26c34a42da6f98204de40c4
sarahlikesoranges <109631514+sarahlikesoranges@users.noreply.github.com>
2022-09-08 11:20:43 +1000

    python-p5
    removed the python and added the p5

---
+++ Tetris/sketch.js
@@ -0,0 +1,15 @@
+function preload(){
+    // anything that has to be loaded before using
+}
+
+function setup(){
+    // set up the screen
+    createCanvas(displayWidth,displayHeight);
+    background(51);
+
+}
+
+function draw(){
+    // anything that moves moves here
+
+}
